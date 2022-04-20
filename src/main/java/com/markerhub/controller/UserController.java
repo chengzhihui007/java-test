ef7911afd5f410cbaaf3597dbb28b415ac218691
chengzhihui007 <469870113@qq.com>
2022-04-20 13:59:17 +0800

    java-teset

---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.markerhub.controller;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.markerhub.common.lang.Result;
+import com.markerhub.entity.Blog;
+import com.markerhub.entity.User;
+import com.markerhub.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2020-05-25
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequiresAuthentication
+    @GetMapping("/index")
+    public Result index() {
+        User user = userService.getById(1L);
+        return Result.succ(user);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user) {
+        return Result.succ(user);
+    }
+
+    @GetMapping("/navlist")
+    public Result list(@RequestParam(defaultValue = "1") Integer currentPage) {
+                    List<Map<String,String>> dataList = new ArrayList<>() ;
+
+            Map<String,String> data = new HashMap<String,String>();
+        data.put("path","/home");
+        data.put("permission","true");
+        data.put("name","cccc");
+        dataList.add(data);
+        return Result.succ(dataList);
+    }
+
+}
